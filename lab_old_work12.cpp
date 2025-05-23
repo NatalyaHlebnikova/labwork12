ceaca8ac719a90b9b9c430b06d39c2f8485b412c
Natalya Hlebnikova <natahleb2006@gmail.com>
2025-05-23 22:56:53 +0300

    add tag_1 array

--- lab_old_work12.cpp
+++ lab_old_work12.cpp
@@ -9,38 +9,6 @@ int main()
 	return 0;
 }
 
-#include "stdio.h"
-int main()
-{	
-	double h=6.5;
-	double r=4.3, V;
-
-	V=3.14*r*r*h; 
-	printf("V = %lf\n",V); 
-	return 0;
-
-}
-
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	double h, r, V;
-
-	cout << "Введіть h: ";
-	cin >> h;
-	cout << "Введіть r: ";
-	cin >> r;
-
-           V=3.14*r*r*h;
-	cout << "V =" << V << endl;
-	system("pause");
-	return 0;
-}
-
-
 #include <iostream>
 using namespace std;
 int main()
@@ -52,7 +20,7 @@ int main()
 	cout << "Введіть r: ";
 	cin >> r;
 
-           s=2*3.14*h*r;
+    s=2*3.14*h*r;
 	cout << "s = " << s << endl;
 	system("pause");
 	return 0;
